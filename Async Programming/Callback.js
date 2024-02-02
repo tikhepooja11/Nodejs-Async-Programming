f0eb05822dd08c32e3af8a4bf2f6575230f75e6f
Pooja Tikhe <47672660+tikhepooja11@users.noreply.github.com>
2024-02-02 17:23:04 +0530

    Update Callback.js

--- Async Programming/Callback.js
+++ Async Programming/Callback.js
@@ -10,3 +10,37 @@ const myFunction = (callbackFunction) => {
 };
 
 myFunction(callbackFunction);
+
+
+
+
+
+
+// another example
+const axios = require('axios');
+
+// Function to fetch data from another server using a callback
+function fetchDataFromServer(url, callback) {
+  // Make an HTTP GET request to the specified URL
+  axios.get(url)
+    .then(response => {
+      // If the request is successful, invoke the callback with the data
+      callback(null, response.data);
+    })
+    .catch(error => {
+      // If there's an error, invoke the callback with the error
+      callback(error, null);
+    });
+}
+
+// Example usage of the fetchDataFromServer function
+const apiUrl = 'https://jsonplaceholder.typicode.com/posts/1';
+
+fetchDataFromServer(apiUrl, (error, data) => {
+  if (error) {
+    console.error('Error fetching data:', error);
+  } else {
+    console.log('Data from the server:', data);
+  }
+});
+
