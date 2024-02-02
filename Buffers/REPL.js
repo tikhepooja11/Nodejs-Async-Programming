f3f354b013881c292e282d81a4182140e9d0426c
Pooja Tikhe <47672660+tikhepooja11@users.noreply.github.com>
2024-02-02 21:02:05 +0530

    Create REPL.js

---
+++ Buffers/REPL.js
@@ -0,0 +1,6 @@
+REPL stands for Read Eval Print Loop, and it represents a computer environment
+
+Read - reads a user input, parses input into javascript datastructure, stores into a memory
+eval - takes and evaluates the data strucutre.
+print - prints the user input on console
+loop - repeats the above procedure until the user presses ctrl+c twice.
